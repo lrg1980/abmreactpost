78fa232f7618c2376e11fc6cec0e246b33471b29
lrg1980 <admin@binweb.net>
2019-04-19 21:01:23 -0300

    Agregando eliminar entrada

--- src/componentes/Post.js
+++ src/componentes/Post.js
@@ -1,7 +1,32 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import swal from 'sweetalert2';
 
 class Post extends Component {
+
+     confirmarEliminar = () => {
+          const { id } = this.props.info;
+          
+          swal.fire({
+               title: 'Está seguro?',
+               text: "No va a poder revertir la acción!",
+               type: 'warning',
+               showCancelButton: true,
+               confirmButtonColor: '#3085d6',
+               cancelButtonColor: '#d33',
+               confirmButtonText: 'Si, eliminar!'
+          }).then((result) => {
+               if (result.value) {
+                    this.props.borrarPost(id)
+                    swal.fire(
+                    'Eliminado!',
+                    'Su entrada ha sido eliminada.',
+                    'exitosamente'
+                    )
+               }
+          })
+     }
+     
      
      render() { 
           const { id, title } = this.props.info;
@@ -11,7 +36,7 @@ class Post extends Component {
                     <td>{title}</td>
                     <td>
                          <Link to={`/post/${id}`} className="btn btn-primary">Ver</Link>
-                         <button onClick={ () => this.props.borrarPost(id) } type="button" className="btn btn-danger">Borrar</button>
+                         <button onClick={ this.confirmarEliminar } type="button" className="btn btn-danger">Borrar</button>
                     </td>
                </tr>
           );
